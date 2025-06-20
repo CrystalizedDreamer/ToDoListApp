4dc06313f247cf6c71a65a80e171f01585ffc3ed
William <ware.william.joe@gmail.com>
2025-06-19 23:19:51 -0400

    Initialization of a new Git repository This is the initial commit of the repository. It includes the following files: - Readme.txt: A brief description of the project. - src/Main.py: The main Python script that runs the application.
    In addition, this will contain the my updates going forward.

---
+++ src/Main.py
@@ -0,0 +1,15 @@
+def main():
+    
+    
+    while True:
+        print("Welcome to your personal task manager...")
+        print("Before we begin, what is your name?")
+        name = input("Enter your name: ")
+        print(f"Hello, {name}! Please select an action...")
+        print("1. Add a task")
+        print("2. View tasks")
+        print("3. Delete a task")
+        print("4. Exit")
+        
+        
+main()
